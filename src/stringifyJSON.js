d7d4ef3f606d08f50779ea3d19b144905dce04cb
presw <pmwhitehall@gmail.com>
2019-07-15 15:41:52 -0700

    stringifyJSON complete

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,10 +5,34 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
-//   if (typeof obj === 'string' || typeof obj === 'number' || typeof obj === 'boolean' || obj === null) {
-//     return `${obj}`
-//   }
-//   if (typeof obj === 'undefined' || typeof obj === 'function') {
-//     return null
-//   }
+  if (typeof obj === 'number' || typeof obj === 'boolean') {
+    return `${obj}`;
+  } else if (typeof obj === 'string') {
+    return `"${obj}"`;
+  }
+  if (obj === null) {
+    return `${obj}`;
+  }
+  if (typeof obj === 'undefined' || typeof obj === 'function') {
+    return null;
+  }
+  if (Array.isArray(obj)) {
+    var arr = [];
+    for (var i = 0; i < obj.length; i++) {
+      arr.push(stringifyJSON(obj[i]));
+    }
+    return `[${arr}]`;
+  } else {
+    var outputObj = {};
+    if (Object.keys(obj).length === 0) {
+      return `{}`;
+    }
+    var outputArr = [];
+    for (var key in obj) {
+      if (typeof obj[key] !== 'function' && obj[key] !== undefined) {
+        outputArr.push(stringifyJSON(key) + `:` + stringifyJSON(obj[key]));
+      }
+    }
+    return `{${outputArr}}`;
+  }
 };
