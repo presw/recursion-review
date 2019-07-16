2c05ac01f09fbb83e8b0c0c3c49f34e5245d4811
presw <pmwhitehall@gmail.com>
2019-07-15 17:34:10 -0700

    added basic framework

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,40 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+/*  your code here
+  iterate through DOM elements
+    if the element has children
+        pass element to func
+    if the element has className 
+        return element
+  var body;
+  if (arguments.length === 1) {
+    body = document.body;
+  } else {
+    body = arguments[1];
+  }
+  // var body = document.body
+  var output = [];
+
+  for (let i = 0; i < body.length; i++) {
+    if (body[i].hasChildNodes()) {
+      getElementsByClassName(className, body.childNodes[i]);
+    }
+    if (body[i].className === className) {
+      output.push(body[i]);
+    }
+  }
+  
+  Working on what to return
+  return output.concat()
+  
+
+    
+    Goal:
+    build an array of elements that match className
+    then return output.concat(func)
+    
+    body.childNode[i].classList[r]*/
+    
 };
